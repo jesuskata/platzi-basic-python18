ac3e72268a596a8f5ae87d714b9f0b0b86c275bf
jesuskata <rzarate.alex@gmail.com>
2018-10-29 03:36:03 -0600

    Se añadió una interfaz a la app y algunas condicionales interesantes

--- main.py
+++ main.py
@@ -3,8 +3,11 @@ clients = 'pablo,ricardo,'
 def create_client(client_name):
     global clients
 
-    clients += client_name
-    _add_comma()
+    if client_name not in clients:
+        clients += client_name
+        _add_comma()
+    else:
+        print('The client you enter is already on the client\'s list. Please try again')
 
 
 def list_clients():
@@ -18,9 +21,24 @@ def _add_comma():
 
     clients += ','
 
+
+def _print_welcome():
+    print('WELCOME TO VENTAS!')
+    print('*' * 50)
+    print('What would you like to do today?')
+    print('[C]reate client')
+    print('[D]elete client')
+
 if __name__ == '__main__':
-    list_clients()
+    _print_welcome()
 
-    create_client('jesus')
+    command = input()
 
-    list_clients()
+    if command == 'C':
+        client_name = input('What is the client name? ')
+        create_client(client_name)
+        list_clients()
+    elif command == 'D':
+        pass
+    else:
+        print('Invalid command! Please try again')
