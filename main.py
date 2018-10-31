15c1f0512d2b6e1a9b5e89e9c08a316a620ca938
jesuskata <rzarate.alex@gmail.com>
2018-10-30 23:58:30 -0600

    Se ha creado la funcionalidad de update client en la app main.py.

--- main.py
+++ main.py
@@ -16,6 +16,15 @@ def list_clients():
     print(clients)
 
 
+def update_client(client_name, updated_client_name):
+    global clients
+
+    if client_name in clients:
+        clients = clients.replace(client_name + ',', updated_client_name + ',')
+    else:
+        print('Client is not in the clients list')
+
+
 def _add_comma():
     global clients
 
@@ -27,16 +36,28 @@ def _print_welcome():
     print('*' * 50)
     print('What would you like to do today?')
     print('[C]reate client')
+    print('[U]pdate client')
     print('[D]elete client')
+
+
+def _get_client_name():
+    return input('What is the client name? ')
+
 
 if __name__ == '__main__':
     _print_welcome()
 
     command = input()
+    command = command.upper()
 
     if command == 'C':
-        client_name = input('What is the client name? ')
+        client_name = _get_client_name()
         create_client(client_name)
+        list_clients()
+    elif command == 'U':
+        client_name = _get_client_name()
+        updated_client_name = input('What is the updated cliente name? ')
+        update_client(client_name.lower(), updated_client_name)
         list_clients()
     elif command == 'D':
         pass
