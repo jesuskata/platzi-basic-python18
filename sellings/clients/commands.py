5f3df5d14e1a45faa807df028e4798fac1c6517b
jesuskata <rzarate.alex@gmail.com>
2018-11-05 10:37:11 -0600

    Se implementó la opción de update clients en para ejecutarla en el cli

--- sellings/clients/commands.py
+++ sellings/clients/commands.py
@@ -63,10 +63,36 @@ def list(ctx):
 
 
 @clients.command()
+@click.argument(
+    'client_uid',
+    type=str)
 @click.pass_context
 def update(ctx, client_uid):
     """Updates a client"""
-    pass
+    client_service = ClientService(ctx.obj['clients_table'])
+
+    client_list = client_service.list_clients()
+
+    client = [client for client in client_list if client['uid'] == client_uid]
+
+    if client:
+        client = _update_client_flow(Client(**client[0]))
+        client_service.update_client(client)
+
+        click.echo('Client updated')
+    else:
+        click.echo('Client not found')
+
+
+def _update_client_flow(client):
+    click.echo('Leave empty if you don\'t want to modify the value')
+
+    client.name = click.prompt('New name', type=str, default=client.name)
+    client.company = click.prompt('New company', type=str, default=client.company)
+    client.email = click.prompt('New email', type=str, default=client.email)
+    client.position = click.prompt('New position', type=str, default=client.position)
+
+    return client
 
 
 @clients.command()
--- sellings/clients/services.py
+++ sellings/clients/services.py
@@ -31,9 +31,10 @@ class ClientService:
 
         self._save_to_disk(updated_clients)
 
+
     def _save_to_disk(self, clients):
         tmp_table_name = self.table_name + '.tmp'
-        with open(tmp_table_name) as f:
+        with open(tmp_table_name, mode='a') as f:
             writer = csv.DictWriter(f, fieldnames=Client.schema())
             writer.writerows(clients)
 
